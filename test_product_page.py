35b73ed428de2b0934bc080cbd16e4a8efab3c8e
AnnaGalieva1 <109988094+AnnaGalieva1@users.noreply.github.com>
2024-06-24 17:21:02 +0500

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -29,7 +29,7 @@ pages.LoginPageLocators
 )
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser, link):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
     page = pages.ProductPage(browser, link)
     page.open()
